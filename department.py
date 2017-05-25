8d6ed9fbc3201a4c12b7035421f1554ff9fa7289
Jane Sandberg <sandbej@linnbenton.edu>
2017-05-25 15:20:28 -0700

    adding logging capability

--- department.py
+++ department.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
 config = yaml.safe_load(open('conf/email.yml'))
+config.update(yaml.safe_load(open('conf/output.yml')))
 
 class Department:
 	# name is a string containing the name of the department
@@ -46,11 +47,13 @@ class Department:
 		html = html + '<p>We\'ve recently added some books/materials to our collection that we think you might be interested in:</p>'
 		images_attached = 0
 		book_number = 0
-		while ((images_attached < config['max_items_per_email']) and (book_number < len(self.books_of_interest))):
-			if hasattr(self.books_of_interest[book_number], 'cover_image_url'):
-				html = html + self.books_of_interest[book_number].to_html()
-				images_attached = images_attached + 1 # increase the first loop by one and continue until you reach 8
-			book_number = book_number + 1 # if the default cover image is the same as the first URL image then increase the loop by one and conitneu the loop
+		with open(config['log_path'], 'a') as log_file:
+			while ((images_attached < config['max_items_per_email']) and (book_number < len(self.books_of_interest))):
+				if hasattr(self.books_of_interest[book_number], 'cover_image_url'):
+					html = html + self.books_of_interest[book_number].to_html()
+					log_file.write(self.books_of_interest[book_number].title + " emailed to " + self.name + "\n")
+					images_attached = images_attached + 1 # increase the first loop by one and continue until you reach 8
+				book_number = book_number + 1 # if the default cover image is the same as the first URL image then increase the loop by one and conitneu the loop
 
 		html = html + '<p>A more complete listing is available at http://notifications.lbcc.linnlibraries.org. Please contact <a href="mailto:hawkinr@linnbenton.edu">Richenda Hawkins</a>'
 		html = html + ' if you would like us to purchase other materials for our collection.<p>'
@@ -70,11 +73,12 @@ class SpanishInterestGroup(Department):
 	# Simpler init, becaues we don't need a call_number_match or name
 	def __init__(self, email):
 		self.email = email
+		self.name = "Spanish Interest Group"
 		self.books_of_interest = []
 
 	def is_interested_in(self, book):
-		if hasattr('book', 'language'):
-			if 'Spanish' == book.language:
+		if hasattr(book, 'language'):
+			if 'es' == book.language:
 				return True
 		return False
 
